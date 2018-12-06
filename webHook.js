529f531b17e881b1a1e139b09ff37ef54b7f4123
olegk <oleg_kleyman@hotmail.com>
2018-12-06 16:15:05 +0200

    .

--- webHook.js
+++ webHook.js
@@ -1,5 +1,5 @@
-import express from 'express';
-//import bodyParser from 'body-parser';
+//import express from 'express';
+var express = require('express');
 
 const app = express();
 app.use(express.json());
