fcae157cd94c8b713782be19a409688667eddbaa
olegk <oleg_kleyman@hotmail.com>
2018-12-10 16:03:20 +0200

    v2.0.0

--- webHook.js
+++ webHook.js
@@ -1,32 +1,50 @@
 var express = require('express');
 var fetch = require('node-fetch');
 var HttpsProxyAgent = require('https-proxy-agent');
+var fs = require('fs');
+
+const BUNDLE_FILE_NAME = 'bundle.js';
 
 const app = express();
 app.use(express.json());
 
 const port = 4567;
 
-app.post('/payload', (req, res) => {
-  const headCommit = req.body.head_commit;
-  const mofiledFiles = headCommit.modified;
-  console.log(`Modified files: ${mofiledFiles}`);
+function pFetch(url) {
+  const proxyServer =  process.env.HTTP_PROXY ||
+                      'http://forticache:8080';
+  return fetch(url, {
+      agent: new HttpsProxyAgent(proxyServer)
+  });
+}
 
-  var headers = {
-    Accept: "application/vnd.github.v3+json"
-  }
-  fetch('https://api.github.com/repos/Tel-Aviv/mob_closed_streets/contents/webHook.js',
-        {
-          method: 'GET',
-          headers: headers,
-          agent: new HttpsProxyAgent('http://forticache:8080')
-        })
+app.post('/payload', (req, res) => {
+
+  const assetsURL = req.body.release.assets_url;
+  console.log(`Downloading from ${assetsURL}`);
+
+  pFetch(assetsURL)
   .then( res => res.json())
   .then( json => {
-    console.log(json.content);
-    console.log(Buffer.from(json.content, 'base64').toString('ascii'));
+    console.log(`Assets list downloaded`);
+    const bundleItem = json.find( item => {
+      return item.name == BUNDLE_FILE_NAME
+    });
+    if( bundleItem ) {
+      return pFetch(bundleItem.browser_download_url);
+    }
   })
-  .catch( err => {
+  .then( res => res.text() )
+  .then( content => {
+    fs.writeFile(BUNDLE_FILE_NAME, content, err => {
+      if( err )
+        console.error(`Error writing file: ${err}`);
+      else
+        console.log(`Successufully saved`);
+    });
+
+  })
+  .catch(err => {
     console.error(err);
   });
 
